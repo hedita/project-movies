b1baeedb31201135c95e1b535abef0f21bdd29ce
Hedita <hedi.takrami@gmail.com>
2022-10-10 01:27:21 +0330

    fetch the api and shoe it in page

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,25 @@ async function getMovies() {
   const result = await fetch("http://localhost:8080/");
   const resultJson = await result.json();
   const {results} = resultJson;
+  const movieList = document.getElementById("movie-list");
+  const imgSrc = "./assets/test.jpg";
+  listHtml = "";
 
-  const movieName = document.getElementById("movie-name");
-  const movieTitle = resultJson.results[0].title;
-  movieName.innerHTML = movieTitle;
-
-  const movieRelease = document.getElementById("movie-release");
-  const movieReleaseDate = resultJson.results[0].release_date;
-  movieRelease.innerHTML = movieReleaseDate;
-  
+  results.forEach(movie => {
+    const movieTitle = resultJson.results[0].title;
+    const movieReleaseDate = resultJson.results[0].release_date;
+    listHtml += `<li class="movie-item">
+    <a href="./movie.html">
+      <img id="img" class="img" src=${imgSrc}>
+      <section class="movie-details">
+        <h2 id="movie-name" class="movie-name">${movie.title}</h2>
+        <p id="movie-release" class="movie-release">${movie.release_date}</p>
+      </section>
+    </a>
+  </li>`
+  console.log(movie.title)
+  });
+  movieList.innerHTML = listHtml;
 }
 
 getMovies();
