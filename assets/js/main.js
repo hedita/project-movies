dce5e2d33bf3873c5acf2a1720776cdc2a2d7826
Hedita <hedi.takrami@gmail.com>
2022-11-17 00:31:17 +0330

    fix some bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,19 @@
+const apiKey = "9f120f0e95e5cedf05a78abda9ef8477";
+
 async function getMovies() {
-  const result = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=9f120f0e95e5cedf05a78abda9ef8477&language=en-US&page=1");
+  const result = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`);
   const resultJson = await result.json();
   const {results: movies} = resultJson;
   const movieList = document.getElementById("movie-list");
   let listHtml = "";
 
   movies.forEach(movie => {
-    listHtml += `<li id="movie-item" class="movie-item">
-    <a id="movie-link" href="./movie.html?id=${movie.id}">
-      <img id="img" class="img" src="https://image.tmdb.org/t/p/w342/${movie.poster_path}" />
+    listHtml += `<li class="movie-item">
+    <a href="./movie.html?id=${movie.id}">
+      <img class="img" src="https://image.tmdb.org/t/p/w342/${movie.poster_path}" />
       <section class="movie-details">
-        <h2 id="movie-name" class="movie-name">${movie.title}</h2>
-        <p id="movie-release" class="movie-release">${movie.release_date}</p>
+        <h2 class="movie-name">${movie.title}</h2>
+        <p class="movie-release">${movie.release_date}</p>
       </section>
     </a>
   </li>`;
