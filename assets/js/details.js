a06368f7b6790db7a71bb7c72263142cd2c2f910
Hedita <hedi.takrami@gmail.com>
2022-11-19 15:32:05 +0330

    rename the property

--- assets/js/details.js
+++ assets/js/details.js
@@ -5,17 +5,17 @@
   
   async function getMovieDetails () {
    const result = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`);
-   const {poster_path: img, title, vote_average, overview} = await result.json();
+   const {poster_path: posterPath, title, voteAverage, overview} = await result.json();
 
    document.body.style = ` background-image: linear-gradient(
       var(--primary-gradient-color),
       var(--secondery-gradient-color)),
-      url("https://image.tmdb.org/t/p/w342/${img}");`
+      url("https://image.tmdb.org/t/p/w342/${posterPath}");`
 
-  detailsContainer.innerHTML = `<img id="movie-img" class="img" src="https://image.tmdb.org/t/p/w342/${img}" />
+  detailsContainer.innerHTML = `<img id="movie-img" class="img" src="https://image.tmdb.org/t/p/w342/${posterPath}" />
    <figcaption class="details">
      <h2 id="movie-name" class="movie-name"${title}></h2>
-     <p id="rate" class="rate">${vote_average}</p>
+     <p id="rate" class="rate">${voteAverage}</p>
      <p id="over-view" class="summary">${overview}</p>
    </figcaption>` 
 }
